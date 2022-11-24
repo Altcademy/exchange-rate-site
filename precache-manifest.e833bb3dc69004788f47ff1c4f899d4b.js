c048935dcb42ade9c360b1f03442d20071980337
David Young <xiaoyang88@gmail.com>
2022-11-24 22:57:56 +0800

    Updates

---
+++ precache-manifest.e833bb3dc69004788f47ff1c4f899d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413cce35660eafaa9c66ee1321dd3f2b",
+    "url": "/exchange-rate-site/index.html"
+  },
+  {
+    "revision": "55363196ad512764e559",
+    "url": "/exchange-rate-site/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2c4853b64598b6c8b935",
+    "url": "/exchange-rate-site/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "55363196ad512764e559",
+    "url": "/exchange-rate-site/static/js/2.04074917.chunk.js"
+  },
+  {
+    "revision": "2c4853b64598b6c8b935",
+    "url": "/exchange-rate-site/static/js/main.0052306e.chunk.js"
+  },
+  {
+    "revision": "9c93bf34dd35b045b197",
+    "url": "/exchange-rate-site/static/js/runtime~main.aabd9a97.js"
+  }
+]);
